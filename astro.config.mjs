1592843327ce75a9c15a1c1246ae922b2f4616c6
Jakub Sobel <qbasob@gmail.com>
2024-08-07 22:18:41 +0200

    Test Vercel ImageOptimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel()
+  adapter: vercel({
+    imageService: true,
+    imagesConfig: {
+      sizes: [320, 640, 960, 1200, 1600, 1800],
+      domains: [],
+    },
+  }),
 });
