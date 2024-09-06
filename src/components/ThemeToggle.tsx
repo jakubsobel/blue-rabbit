cb7afe6a657825d9de90048ed9b0e0fda2e47102
Jakub Sobel <qbasob@gmail.com>
2024-09-07 00:25:27 +0200

    Add theme picker view transitions

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,19 +1,77 @@
+// TODO: Move theme logic to utils
 export default function ThemeToggle() {
   const onClickLight = () => {
-    document.documentElement.classList.remove("dark");
-    localStorage.setItem("theme", "light");
+    // If switching to same theme, do nothing
+    if (localStorage.getItem("theme") === "light") return;
+    // If switching from light system theme to light theme, set just localStorage entry
+    if (
+      !localStorage.getItem("theme") &&
+      matchMedia("(prefers-color-scheme: light)").matches
+    ) {
+      localStorage.setItem("theme", "light");
+      return;
+    }
+    // @ts-ignore
+    document.startViewTransition({
+      update: () => {
+        document.documentElement.classList.remove("dark");
+        localStorage.setItem("theme", "light");
+      },
+      types: ["theme"],
+    });
   };
 
   const onClickDark = () => {
-    document.documentElement.classList.add("dark");
-    localStorage.setItem("theme", "dark");
+    // If switching to same theme, do nothing
+    if (localStorage.getItem("theme") === "dark") return;
+    // If switching from dark system theme to dark theme, set just localStorage entry
+    if (
+      !localStorage.getItem("theme") &&
+      matchMedia("(prefers-color-scheme: dark)").matches
+    ) {
+      localStorage.setItem("theme", "dark");
+      return;
+    }
+
+    // @ts-ignore
+    document.startViewTransition({
+      update: () => {
+        document.documentElement.classList.add("dark");
+        localStorage.setItem("theme", "dark");
+      },
+      types: ["theme"],
+    });
   };
 
   const onClickSystem = () => {
-    matchMedia("(prefers-color-scheme: dark)").matches
-      ? document.documentElement.classList.add("dark")
-      : document.documentElement.classList.remove("dark");
-    localStorage.removeItem("theme");
+    // If switching to same theme, do nothing
+    if (!localStorage.getItem("theme")) return;
+    // If switching from dark theme to dark system theme, remove just localStorage entry
+    if (
+      localStorage.getItem("theme") === "dark" &&
+      matchMedia("(prefers-color-scheme: dark)").matches
+    ) {
+      localStorage.removeItem("theme");
+      return;
+    }
+    // If switching from light theme to light system theme, remove just localStorage entry
+    if (
+      localStorage.getItem("theme") === "light" &&
+      matchMedia("(prefers-color-scheme: light)").matches
+    ) {
+      localStorage.removeItem("theme");
+      return;
+    }
+    // @ts-ignore
+    document.startViewTransition({
+      update: () => {
+        matchMedia("(prefers-color-scheme: dark)").matches
+          ? document.documentElement.classList.add("dark")
+          : document.documentElement.classList.remove("dark");
+        localStorage.removeItem("theme");
+      },
+      types: ["theme"],
+    });
   };
 
   return (
